b9f0a33aa1bf64a8593400dad2c07736dc9151e0
Jens Rogalli <jens.rogalli@gmx.de>
2018-02-22 23:35:29 +0100

    extracted Test Util Function in own class

--- src/test/java/hudson/plugins/filesystem_scm/FolderDiffTest2.java
+++ src/test/java/hudson/plugins/filesystem_scm/FolderDiffTest2.java
@@ -2,56 +2,47 @@ package hudson.plugins.filesystem_scm;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.junit.Test;;
 
 public class FolderDiffTest2 {
 
-    private static String createPlatformDependendPath(String... parts) {
-        String path = "";
-        for (int i = 0; i < parts.length - 1; i++) {
-            path = path.concat(parts[i]).concat(File.separator);
-        }
-        path = path.concat(parts[parts.length - 1]);
-        return path;
-    }
-
     @Test
     public void GetRelative_correctFileAndFolder_Subpath() throws IOException {
-        String folder = createPlatformDependendPath("c:", "tmp");
-        String expected = createPlatformDependendPath("abc", "qq", "qq.java");
-        String actual = FolderDiff.getRelativeName(createPlatformDependendPath(folder, expected), folder);
+        String folder = TestUtils.createPlatformDependendPath("c:", "tmp");
+        String expected = TestUtils.createPlatformDependendPath("abc", "qq", "qq.java");
+        String actual = FolderDiff.getRelativeName(TestUtils.createPlatformDependendPath(folder, expected), folder);
         assertEquals(expected, actual);
     }
 
     @Test
     public void GetRelative_correctFileAndDrive_Subpath() throws IOException {
-        String folder = createPlatformDependendPath("c:");
-        String expected = createPlatformDependendPath("tmp", "abc", "qq", "qq.java");
-        String actual = FolderDiff.getRelativeName(createPlatformDependendPath(folder, expected), folder);
+        String folder = TestUtils.createPlatformDependendPath("c:");
+        String expected = TestUtils.createPlatformDependendPath("tmp", "abc", "qq", "qq.java");
+        String actual = FolderDiff.getRelativeName(TestUtils.createPlatformDependendPath(folder, expected), folder);
         assertEquals(expected, actual);
     }
 
     @Test(expected = IOException.class)
     public void GetRelative_FileAndFolderWithALetterMissingInName_Exception() throws IOException {
-        String folder = createPlatformDependendPath("c:", "tm");
-        String actual = FolderDiff.getRelativeName(createPlatformDependendPath("c:", "tmp", "abc", "qq", "qq.java"),
-                folder);
+        String folder = TestUtils.createPlatformDependendPath("c:", "tm");
+        String actual = FolderDiff
+                .getRelativeName(TestUtils.createPlatformDependendPath("c:", "tmp", "abc", "qq", "qq.java"), folder);
     }
 
     @Test(expected = IOException.class)
     public void GetRelative_FileAndFolderNotParentOfFile_Exception() throws IOException {
-        String folder = createPlatformDependendPath("c:", "def");
-        String file = createPlatformDependendPath("c:", "tmp", "abc", "qq", "qq.java");
+        String folder = TestUtils.createPlatformDependendPath("c:", "def");
+        String file = TestUtils.createPlatformDependendPath("c:", "tmp", "abc", "qq", "qq.java");
         String x = FolderDiff.getRelativeName(file, folder);
     }
 
     @Test(expected = IOException.class)
     public void GetRelative_FileAndFolderWithPathSeperatorAppended_Exception() throws IOException {
-        String folder = createPlatformDependendPath("c:", "tmp");
-        String expected = createPlatformDependendPath("abc", "qq", "qq.java");
-        String actual = FolderDiff.getRelativeName(createPlatformDependendPath(folder, expected), folder.concat("//"));
+        String folder = TestUtils.createPlatformDependendPath("c:", "tmp");
+        String expected = TestUtils.createPlatformDependendPath("abc", "qq", "qq.java");
+        String actual = FolderDiff.getRelativeName(TestUtils.createPlatformDependendPath(folder, expected),
+                folder.concat("//"));
     }
 }
